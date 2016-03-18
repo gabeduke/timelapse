8dcb465aac8d458600c0f53d2b1f491286d680f4
gabeduke <gduke@bullhorn.com>
2016-03-18 01:38:30 -0400

    adding bash script for image stacking * using enfuse to blend and stack images

--- bash/timelapse.bash
+++ bash/timelapse.bash
@@ -5,9 +5,14 @@ FPS=${2:-24}
 DIRECTORY=export
 # JPEG=${3:-false}
 
-function export {
+function count {
+    ls *.CR2 -l | wc -l
+}
+
+function export() {
     for i in $( ls *.CR2 ); do
         darktable-cli $i $i.xmp $DIRECTORY/$i.jpg
+        printf 1++"/"$1
     done
 }
 
@@ -19,6 +24,7 @@ function listFiles {
 # render
 function render () {
     cd $DIRECTORY && mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=21600000 -o ~/Videos/$1.avi -mf type=jpeg:fps=$2 mf://@files.txt -vf scale=1920:1080
+    notify-send -t 5000 "Your timelapse has finished rendering and is located in ~/Videos"
 }
 
 function cleanup {
@@ -39,7 +45,7 @@ function jpegDeflicker {
 }
 
 if [ ! -d "$DIRECTORY" ]; then
-  export
+  export count
   listFiles
   if [ ! $JPEG = "false" ]; then
       jpegDeflicker
