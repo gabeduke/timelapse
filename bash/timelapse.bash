4967fc688380044c9d5175ef709718bc0024ef71
gabeduke <gduke@bullhorn.com>
2016-03-26 02:27:17 -0400

    Timelapse.bash *adding 3rd option for high quality render *adding counter

--- bash/timelapse.bash
+++ bash/timelapse.bash
@@ -2,17 +2,22 @@
 
 NAME=${1:-rendered}
 FPS=${2:-24}
+QUALITY=${3:-normal}
 DIRECTORY=export
 # JPEG=${3:-false}
 
-function count {
-    ls *.CR2 -l | wc -l
-}
+if [ $3 = "high" ]; then
+    LAVCOPTS="vcodec=mpeg4:vbitrate=21600000:trell:mbd=2:dc=10"
+else
+    LAVCOPTS="vcodec=mpeg4:vbitrate=21600000"
+    fi
 
 function export() {
+TOTAL="$(ls *.CR2 -l | wc -l)"
     for i in $( ls *.CR2 ); do
         darktable-cli $i $i.xmp $DIRECTORY/$i.jpg
-        printf 1++"/"$1
+        COUNTER=$((COUNTER+1))
+        printf "%s/%s\n" "$COUNTER" "$TOTAL"
     done
 }
 
@@ -23,7 +28,7 @@ function listFiles {
 
 # render
 function render () {
-    cd $DIRECTORY && mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=21600000 -o ~/Videos/$1.avi -mf type=jpeg:fps=$2 mf://@files.txt -vf scale=1920:1080
+    cd $DIRECTORY && mencoder -nosound -ovc lavc -lavcopts $3 -o ~/Videos/$1.avi -mf type=jpeg:fps=$2 mf://@files.txt -vf scale=1920:1080
     notify-send -t 5000 "Your timelapse has finished rendering and is located in ~/Videos"
 }
 
@@ -38,19 +43,18 @@ exit
 }
 
 # jpeg deflicker
-
 function jpegDeflicker {
     ./home/gabeduke/repos/timelapse-scripts/perl/deflicker.pl -v
     $DIRECTORY=Deflicker
 }
 
 if [ ! -d "$DIRECTORY" ]; then
-  export count
+  export
   listFiles
   if [ ! $JPEG = "false" ]; then
       jpegDeflicker
   fi
-  render $NAME $FPS
+  render $NAME $FPS $LAVCOPTS
   quit
 else
   render $NAME $FPS
