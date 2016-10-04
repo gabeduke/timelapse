45f3905b162f8a478c1947e113406b18a1b73031
Gabriel Duke <gabeduke@gmail.com>
2016-10-04 10:58:51 -0400

    Update moisture.py
    adding time logging

--- wioLink/moisture.py
+++ wioLink/moisture.py
@@ -1,19 +1,23 @@
 import requests
+import json
+import datetime
 from pymongo import MongoClient
 
-#Mongo setup
-cred = "gabeduke:garrity@"
-host = "programming.local:"
-port = "27017"
-client = MongoClient("mongodb://" + cred + host + port)
+#Mongo setup##
+client = MongoClient()
 db = client.moisture
 
 server = "https://us.wio.seeed.io/v1"
 token = "?access_token=eb306fbdff26c105bf271f7a4c24f91a"
 node = "/node/GroveMoistureA0/moisture"
-moist = requests.get(server + node + token)
+moist = requests.get(server + node + token
 
+##FORMAT JSON##
+today = datetime.datetime.now().strftime("%Y-%m-%d %H")
 reading = moist.json()
-insert = db.readings.insert_one(reading)
 
-print insert.inserted_id
+data = {'reading': [reading]}
+data['reading'].insert(0, {'date': today})
+
+##INSERT DATA TO MONGODB##
+insert = db.readings.insert_one(data)
