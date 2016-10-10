73cc728b0e17a84aedb702f1b50e5f3dda4acee7
Duke <gduke@bullhorn.com>
2016-10-10 11:10:45 -0400

    adding air quality node

--- wioLink/sensors.py
+++ wioLink/sensors.py
@@ -12,16 +12,22 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-##GET data##
-wio_url = "https://us.wio.seeed.io/v1"
-wio_token = "?access_token=eb306fbdff26c105bf271f7a4c24f91a"
-wio_moistureNode = "/node/GroveMoistureA0/moisture"
-wio_humidityNode = "/node/GroveTempHumD0/humidity"
-wio_tempNode = "/node/GroveTempHumD0/temperature_f"
+def formatUrl(node):
+    requests.get(wio_url + node + wio_token)
+    return requests
 
-moisture = requests.get(wio_url + wio_moistureNode + wio_token)
-humidity = requests.get(wio_url + wio_humidityNode + wio_token)
-temp = requests.get(wio_url + wio_tempNode + wio_token)
+##GET data##
+wio_url = "https://us.wio.seeed.io"
+wio_token = "?access_token=eb306fbdff26c105bf271f7a4c24f91a"
+wio_moistureNode = "/v1/node/GroveMoistureA0/moisture"
+wio_humidityNode = "/v1/node/GroveTempHumD0/humidity"
+wio_tempNode = "/v1/node/GroveTempHumD0/temperature_f"
+wio_airQuality = "/v1/node/GroveAirqualityA0/quality"
+
+moisture = formatUrl(wio_moistureNode)
+humidity = formatUrl(wio_humidityNode)
+temp = formatUrl(wio_tempNode)
+airQuality = formatUrl(wio_airQuality)
 
 ##FORMAT JSON##
 today = datetime.datetime.now().strftime("%Y-%m-%d %H")
@@ -43,7 +49,7 @@ temp = value['fahrenheit_degree']
 ##POST to Thinkspeak##
 thingspeak_url = "https://api.thingspeak.com/update.json"
 thingspeak_apiKey = "Y35LZQ4BTRZBMN37"
-payload = {'api_key': thingspeak_apiKey, 'field2': moisture, 'field3': humidity, 'field4': temp}
+payload = {'api_key': thingspeak_apiKey, 'field5': airQuality, 'field2': moisture, 'field3': humidity, 'field4': temp}
 
 req = requests.post(thingspeak_url, data=payload)
 print(req.text)
