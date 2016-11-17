c328a28bed5a740f89ac0ccd01b9a31daf3f7705
Dukeman <gduke@bullhorn.com>
2016-11-16 22:21:48 -0600

    building out wiolink

---
+++ wioLink/properties.py
@@ -0,0 +1,13 @@
+import os
+
+##GET data##
+wio_url = "https://us.wio.seeed.io"
+wio1_token = "?access_token=eb306fbdff26c105bf271f7a4c24f91a"
+wio_pete_token = "?access_token=5d56c05c82d1e2453f9dcaa0bb59144c"
+wio_moistureNode = "/v1/node/GroveMoistureA0/moisture"
+wio_humidityNode = "/v1/node/GroveTempHumD0/humidity"
+wio_tempNode = "/v1/node/GroveTempHumD0/temperature_f"
+wio_airQuality = "/v1/node/GroveAirqualityA0/quality"
+
+thingspeak_url = "https://api.thingspeak.com/update.json"
+thingspeak_apiKey = "Y35LZQ4BTRZBMN37"
--- wioLink/sensors.py
+++ wioLink/sensors.py
@@ -1,59 +1,34 @@
-#!usr/bin/env python  
+#!usr/bin/env python
+from enum import Enum
+from properties import *
 
 import requests
 import json
-import datetime
 import logging
 
-##Enable Logging##
+
+class Nodes(Enum):
+    moisture = wio_moistureNode
+
+
+# # Enable Logging # #
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-def formatUrl(node):
 
-    return requests.get
+def format_url(node, token):
+    req = requests.get(wio_url + node.value + token)
+    return json.loads(node.name, req)
 
-def formatJson(x):
-    reading = x.json()
-    dump = json.dumps(reading)
-    return json.loads(dump)
 
-##GET data##
-wio_url = "https://us.wio.seeed.io"
-wio1_token = "?access_token=eb306fbdff26c105bf271f7a4c24f91a"
-wio2_token = "?access_token=e8240174cbe8bf574cf9ef119fc1bfa6"
-wio_moistureNode = "/v1/node/GroveMoistureA0/moisture"
-wio_humidityNode = "/v1/node/GroveTempHumD0/humidity"
-wio_tempNode = "/v1/node/GroveTempHumD0/temperature_f"
-wio_airQuality = "/v1/node/GroveAirqualityA0/quality"
+def post_data_to_thinkspeak(fieldName, json):
+    payload = {'api_key': thingspeak_apiKey, fieldName.name: json}
+    req = requests.post(thingspeak_url, data=payload)
+    print(req.text)
 
-moisture = requests.get(wio_url + wio_moistureNode + wio1_token)
-humidity = requests.get(wio_url + wio_humidityNode + wio1_token)
-temp = requests.get(wio_url + wio_tempNode + wio1_token)
-airQuality = requests.get(wio_url + wio_airQuality + wio2_token)
-
-##FORMAT JSON##
-today = datetime.datetime.now().strftime("%Y-%m-%d %H")
-
-value = formatJson(moisture)
-moisture = value['moisture']
-
-value = formatJson(humidity)
-humidity = value['humidity']
-
-value = formatJson(temp)
-temp = value['fahrenheit_degree']
-
-value = formatJson(airQuality)
-airQuality = value['quality']
-
-##POST to Thinkspeak##
-thingspeak_url = "https://api.thingspeak.com/update.json"
-thingspeak_apiKey = "Y35LZQ4BTRZBMN37"
-payload = {'api_key': thingspeak_apiKey, 'field5': airQuality, 'field2': moisture, 'field3': humidity, 'field4': temp}
-
-req = requests.post(thingspeak_url, data=payload)
-print(req.text)
+node = Nodes.moisture
+wioStamp = format_url(node, wio_pete_token)
+post_data_to_thinkspeak(node, wioStamp)
