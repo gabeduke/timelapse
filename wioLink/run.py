33a02a76c48b3066aeb6394121569cb9b3239370
Dukeman <gduke@bullhorn.com>
2016-11-16 23:30:00 -0600

    squashing

--- wioLink/run.py
+++ wioLink/run.py
@@ -1,5 +1,5 @@
-from wioLink.properties import *
-from wioLink.functions import get_wio_sensor_data, post_data_to_thinkspeak
+from properties import *
+from functions import get_wio_sensor_data, post_data_to_thinkspeak
 
 wioStamp = get_wio_sensor_data(NodeProperties.moisture, wio_pete_token)
 post_data_to_thinkspeak(NodeMap.moisture, wioStamp)
