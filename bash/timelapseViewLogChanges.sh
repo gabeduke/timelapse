e0cb81e26318a8cf7176f10cc82fbf96e91d34f1
Dukeman <gduke@bullhorn.com>
2016-11-24 23:32:55 -0500

    removing emailer

--- bash/timelapseViewLogChanges.sh
+++ bash/timelapseViewLogChanges.sh
@@ -2,13 +2,15 @@
 
 mkdir tmpdir
 git clone --bare https://github.com/timelapseplus/VIEW.git tmpdir
+
 pushd tmpdir
 log=$(git log --date=iso \
     --since=`date --date='yesterday' +%Y-%m-%d` \
     --until=`date --date='tomorrow' +%Y-%m-%d` \
     --summary --show-notes --oneline --date-order)
-popd
-rm -rf tmpdir
+
 echo "$log" >> "tmpFile.txt"
-mutt -s "Timelapse VIEW Log Changes" gabeduke@gmail.com < tmpFile.txt
-rm tmpFile.txt
+popd
+
+rm -rf tmpdir
+
